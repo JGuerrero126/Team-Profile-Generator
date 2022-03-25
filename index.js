b16282ec0a185868e0495930df76a470ba7b79a7
Jerry Guerrero <jerry1996714@gmail.com>
2022-03-25 02:07:17 -0700

    Got mostly everything done, just styling left and validation left

---
+++ index.js
@@ -0,0 +1,139 @@
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+
+const {
+  generateEngineerCard,
+  generateInternCard,
+  generateManagerCard,
+  baseHTML,
+} = require("./src/htmlGen");
+
+const teamMemberHTMLArr = [];
+
+function init() {
+  function managerCreate() {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "name",
+          message: "What is your Manager's name?",
+        },
+        {
+          type: "input",
+          name: "email",
+          message: "What is their email?",
+        },
+        {
+          type: "input",
+          name: "id",
+          message: "What is the Manger's id number?",
+        },
+        {
+          type: "input",
+          name: "officeNumber",
+          message: "What is the Office Number?",
+        },
+      ])
+      .then(({ name, id, email, officeNumber }) => {
+        const manager = new Manager(id, name, email, officeNumber);
+        teamMemberHTMLArr.push(generateManagerCard(manager));
+        mainMenu();
+      });
+  }
+  function mainMenu() {
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          name: "position",
+          choices: ["Engineer", "Intern", "I'm all done!"],
+          message: "Would you like to add another Employee?",
+        },
+      ])
+      .then((answers) => {
+        switch (answers.position) {
+          case "Engineer":
+            return engineerCreate();
+          case "Intern":
+            return internCreate();
+          default:
+            return generateHTML();
+        }
+      });
+  }
+  function engineerCreate() {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "name",
+          message: "What is the Engineer's name?",
+        },
+        {
+          type: "input",
+          name: "email",
+          message: "What is their email?",
+        },
+        {
+          type: "input",
+          name: "id",
+          message: "What is the Engineer's id number?",
+        },
+        {
+          type: "input",
+          name: "github",
+          message: "What is the Engineer's GitHub?",
+        },
+      ])
+      .then(({ name, id, email, github }) => {
+        const engineer = new Engineer(id, name, email, github);
+        teamMemberHTMLArr.push(generateEngineerCard(engineer));
+        mainMenu();
+      });
+  }
+  function internCreate() {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "name",
+          message: "What is the Intern's name?",
+        },
+        {
+          type: "input",
+          name: "email",
+          message: "What is their email?",
+        },
+        {
+          type: "input",
+          name: "id",
+          message: "What is the Intern's id number?",
+        },
+        {
+          type: "input",
+          name: "school",
+          message: "What is the Intern's school?",
+        },
+      ])
+      .then(({ name, id, email, school }) => {
+        const intern = new Intern(id, name, email, school);
+        teamMemberHTMLArr.push(generateInternCard(intern));
+        mainMenu();
+      });
+  }
+  function generateHTML() {
+    fs.writeFile("./dist/index.html", baseHTML(teamMemberHTMLArr), (err) => {
+      err ? console.log(err) : console.log("Your project is good to go!");
+    });
+  }
+
+  managerCreate();
+}
+
+init();
---
+++ src/htmlGen.js
@@ -0,0 +1,81 @@
+const generateManagerCard = (manager) => {
+  return `
+        <div class="card">
+            <h3>${manager.getName()}</h3>
+            <p><i class="fa-solid fa-mug-hot"></i>${manager.getRole()}</p>
+            <div class="card-body">
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">ID: ${manager.getId()}</li>
+                    <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
+                    <li class="list-group-item">Office Number: ${manager.getOfficeNumber()}</li>
+                </ul>
+            </div>
+        </div>
+    `;
+};
+
+const generateEngineerCard = (engineer) => {
+  return `
+        <div class="card">
+              <h3>${engineer.getName()}</h3>
+              <p><i class="fa-solid fa-mug-hot"></i>${engineer.getRole()}</p>
+              <div class="card-body">
+                  <ul class="list-group list-group-flush">
+                      <li class="list-group-item">ID: ${engineer.getId()}</li>
+                      <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
+                      <li class="list-group-item">GitHub: ${engineer.getGithub()}</li>
+                  </ul>
+              </div>
+        </div>
+      `;
+};
+
+const generateInternCard = (intern) => {
+  return `
+          <div class="card">
+                <h3>${intern.getName()}</h3>
+                <p><i class="fa-solid fa-mug-hot"></i>${intern.getRole()}</p>
+                <div class="card-body">
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">ID: ${intern.getId()}</li>
+                        <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
+                        <li class="list-group-item">School: ${intern.getSchool()}</li>
+                    </ul>
+                </div>
+          </div>
+        `;
+};
+
+const baseHTML = (htmlArr) => `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Team Profile Page</title>
+</head>
+<body>
+<header>
+<div class="jumbotron jumbotron-fluid">
+    <h1 class="display-4 heading">My Team</h1>
+</div>
+</header>
+<div id="container">
+<div class="card-deck">
+ ${htmlArr.join(" ")}
+
+</div>
+</div>
+    
+</body>
+</html>
+`;
+
+module.exports = {
+  generateEngineerCard,
+  generateInternCard,
+  generateManagerCard,
+  baseHTML,
+};
